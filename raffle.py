1cd6701c44ddb979419d80bd4b57989b28e61588
Emir Lejlic <emir.lej@gmail.com>
2018-10-22 11:49:25 +0200

    raffle.py: added pair checking function and shuffling the two lists

--- raffle.py
+++ raffle.py
@@ -13,15 +13,25 @@ def all_names_to_list(pairs):
             names.append(name)
     return(names)
 
+def check_if_names_are_a_pair(x, y, pairs):
+    """ Check if name x and y are a pairs """
+    is_pair = False
+    for k, v in pairs.items():
+        if (x in v) and (y in v):
+            is_pair = True
+    return(is_pair)
+
 
 def main():
     """ Main flow """
     pairs = read_pairs_files("pairs.json")
-    
+    results = []
+
     # Create the givers and receivers lists
     givers, receivers = all_names_to_list(pairs), all_names_to_list(pairs)
 
-
+    # Shuffle the lists in place
+    random.shuffle(givers), random.shuffle(receivers)
 
 
 if __name__ == "__main__":
